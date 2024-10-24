f8f0ec8ec0a37dcb625459a9be8930ae99debcb1
leo <Pestov.962@gmail.com>
2024-10-24 19:15:20 +0300

    fixed poms and added template lab2 (module2)

---
+++ lab2/src/main/java/lab2/Main.java
@@ -0,0 +1,12 @@
+package lab2;
+
+public class Main {
+
+    public static void main(String[] args) {  // Добавлены String[] args
+        hey();
+    }
+
+    public static void hey() {
+        System.out.println("Hello from Lab2!");
+    }
+}
